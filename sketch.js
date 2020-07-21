82aa480f35258068ff229da691da18f3846fb4df
namithkumar <65290789+namithkumar@users.noreply.github.com>
2020-07-21 22:10:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Events = Matter.Events;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var canvas;
+var player1, player2, court, courtImg, ball, ground, basket;
+
+function preload(){
+    courtImg = loadImage("bbcourt.png");
+}
+
+function setup(){
+    canvas = createCanvas(displayWidth, displayHeight);
+    engine = Engine.create(); 
+    world = engine.world;
+
+    player1 = new Player(displayWidth/2-50, displayHeight/2, 5, 5);
+    player1.scale = 0.1;
+    player2 = new Player(displayWidth/2+50, displayHeight/2, 20, 20);
+
+    basket = new Basket(1500, 1000);
+    ball = new Ball(850, 600, 50);
+    slingshot = new SlingShot(ball.body,{x:850, y:600});
+    ground = new Ground(900, height, 3000, 20);
+    
+  
+
+}
+
+function draw(){
+    background(courtImg);
+    Engine.update(engine);
+
+    player1.display();
+    player1.run();
+    player1.jump();
+
+    player2.display();
+    player2.run();
+    player2.opponentjump();
+
+    ball.display();
+    basket.display();
+    ground.display();
+
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+        slingshot.attach(ball.body);
+    }
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(ball.body, {x: mouseX , y: mouseY});
+}
+
+function mouseReleased(){
+    slingshot.fly();
+    player2.opponentjump();
+}
